9122e5b5bbeab35a041ccf3ffd6112bcdb68fd5f
cenv0594 <gwyneth.bradbury@ouce.ox.ac.uk>
2017-05-12 15:01:17 +0100

    added error messages for create table

--- projects/map/views.py
+++ projects/map/views.py
@@ -163,6 +163,19 @@ def servedata():
 @map.route("/projects/map/admin/createtable", methods=['GET', 'POST'])
 def createtable():
     #create a new table either from scratch or from an existing csv
+    tablenames, columnnames = DBA.getTableAndColumnNames()
+
+    if request.form.get("newtablename")=="":
+        return render_template('projects/map/create_table.html',
+                               tablenames=tablenames,columnnames=columnnames,
+                               error="Enter table name")
+    for t in tablenames:
+        if request.form.get("newtablename")==t:
+            return render_template('projects/map/create_table.html',
+                                   tablenames=tablenames,columnnames=columnnames,
+                                   error="Table "+t+" already exists, try a new name")
+
+
     if request.form.get("source") == "emptytable":
         DBA.createEmptyTable(request.form.get("newtablename"))
     else:
@@ -170,26 +183,29 @@ def createtable():
         # check if the post request has the file part
         if 'file' not in request.files:
             print('No file part')
-            return redirect('/projects/map/admin/newtable')
+            return render_template('projects/map/create_table.html',
+                                   tablenames=tablenames,columnnames=columnnames,
+                                   error="No file part, contact admin")
+
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
             print('No selected file')
-            return redirect('/projects/map/admin/newtable')
+            return render_template('projects/map/create_table.html',
+                                   tablenames=tablenames,columnnames=columnnames,
+                                   error="No selected file")
         if file:  # and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             dt = datetime.utcnow().strftime("%Y-%m-%d_%H:%M:%S")
             file.save(os.path.join(uploadfolder, dt + '_' + filename))
             print(filename)
-            p = os.path.join(uploadfolder, filename)
-
-            print("about to upload")
-            # p="/Users/cenv0594/Repositories/dbas/projects/map/data/export.csv"
-            # p = request.form.get("file")
-            print(p)
-            DBA.createTableFromCSV(p,
+            DBA.createTableFromCSV(os.path.join(uploadfolder, filename),
                                    request.form.get("newtablename"))
+
+    return render_template('projects/map/create_table.html',
+                           tablenames=tablenames,columnnames=columnnames,
+                           message="Table "+request.form.get("newtablename")+" created successfully!")
     return redirect('/projects/map/admin/')
 
 

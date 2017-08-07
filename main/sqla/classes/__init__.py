316d3254c0887bd94349d006ae684adb2231626d
David Ford <david.ford@ouce.ox.ac.uk>
2017-08-07 16:29:24 +0100

    Update __init__.py with initial attempt at reworking __init__ to autogenerate classesdict

--- main/sqla/classes/__init__.py
+++ main/sqla/classes/__init__.py
@@ -1,11 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
 from main.sqla.app import db as db
+import inspect
+
+#classesdict={}
+
 import classes_master as db_map
 db_map.init('map')
+# this doesn't work as db_map.Project is not a string - needs to be extracted from globals :(
+#for i in db_map.class_list:
+#   classes_arr=i.split('_')
+#   cn=""
+#   for cl in classes_arr:
+#      cn=cn+cl.capitalize()
+#   classesdict["cls_"+map+"_"+i] = 'db_map.'+cn
+
 import classes_master as db_it_lending_log
 db_it_lending_log.init('it_lending_log')
 import classes_master as db_online_learning
 db_online_learning.init('online_learning')
+
 
 
 classesdict={

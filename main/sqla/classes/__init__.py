b1e2852a170ad9deea4b2174764f78e5b5c54325
David Ford <david.ford@ouce.ox.ac.uk>
2017-08-08 08:16:36 +0100

    Update __init__.py

--- main/sqla/classes/__init__.py
+++ main/sqla/classes/__init__.py
@@ -1,36 +1,35 @@
 from flask_sqlalchemy import SQLAlchemy
 from main.sqla.app import db as db
 import inspect
+import importlib
 
-#classesdict={}
+db_list=['map','it_lending_log','online_learning']
 
-import classes_master as db_map
-db_map.init('map')
-# this doesn't work as db_map.Project is not a string - needs to be extracted from globals :(
-#for i in db_map.class_list:
-#   classes_arr=i.split('_')
-#   cn=""
-#   for cl in classes_arr:
-#      cn=cn+cl.capitalize()
-#   classesdict["cls_"+map+"_"+i] = 'db_map.'+cn
+classesdict={}
+my_db = {}
 
-import classes_master as db_it_lending_log
-db_it_lending_log.init('it_lending_log')
-import classes_master as db_online_learning
-db_online_learning.init('online_learning')
-
+for db_item in db_list:
+  my_db[db_item]=importlib.import_module('.classes_master', __name__)
+  my_db[db_item].init(db_item)
+  # this doesn't work as we need to map to the class, not merely the name of the class!
+  for i in my_db[db_item].class_list:
+     classes_arr=i.split('_')
+     cn=""
+     for cl in classes_arr:
+        cn=cn+cl.capitalize()
+#     classesdict["cls_"+db_item+"_"+i] = 'my_db["'+db_item+'"].'+cn
 
 
 classesdict={
-    'cls_map_project': db_map.Project,
-    'cls_map_testtable': db_map.Testtable,
-    'cls_map_emptytable': db_map.Emptytable,
-    'cls_online_learning_videos': db_online_learning.Videos,
-    'cls_online_learning_tags': db_online_learning.Tags,
-    'cls_online_learning_pages_tags': db_online_learning.PagesTags,
-    'cls_online_learning_topic': db_online_learning.Topic,
-    'cls_online_learning_pages_videos': db_online_learning.PagesVideos,
-    'cls_online_learning_pages': db_online_learning.Pages,
-    'cls_it_lending_log_items': db_it_lending_log.Items,
-    'cls_it_lending_log_log': db_it_lending_log.Log,
+    'cls_map_project': my_db['map'].Project,
+    'cls_map_testtable': my_db['map'].Testtable,
+    'cls_map_emptytable': my_db['map'].Emptytable,
+    'cls_online_learning_videos': my_db['online_learning'].Videos,
+    'cls_online_learning_tags': my_db['online_learning'].Tags,
+    'cls_online_learning_pages_tags': my_db['online_learning'].PagesTags,
+    'cls_online_learning_topic': my_db['online_learning'].Topic,
+    'cls_online_learning_pages_videos': my_db['online_learning'].PagesVideos,
+    'cls_online_learning_pages': my_db['online_learning'].Pages,
+    'cls_it_lending_log_items': my_db['it_lending_log'].Items,
+    'cls_it_lending_log_log': my_db['it_lending_log'].Log,
     }

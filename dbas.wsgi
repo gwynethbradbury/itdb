cd186db69ed015f4fed81b723a9c97ca0f9d35c8
SoGE IAAS <itsupport@ouce.ox.ac.uk>
2017-09-06 19:55:40 +0100

    make dispatching work

--- dbas.wsgi
+++ dbas.wsgi
@@ -8,7 +8,44 @@ from flask import request
 import json
 path=__file__[0:-9]
 sys.path.insert(0,path)
-import dispatcher
+
+from threading import Lock
+from werkzeug.wsgi import pop_path_info, extract_path_info, peek_path_info
+
+class PathDispatcher(object):
+
+    def __init__(self, default_app, create_app):
+        self.default_app = default_app
+        self.create_app = create_app
+        self.lock = Lock()
+        self.instances = {}
+
+    def get_application(self, prefix):
+        print "ga_Prefix: "+prefix
+        with self.lock:
+            app = self.instances.get(prefix)
+            if app is None:
+                app = self.create_app(prefix)
+                if app is not None:
+                    self.instances[prefix] = app
+            return app
+
+    def __call__(self, environ, start_response):
+        path_start=peek_path_info(environ)
+        print "Prefix: path_start: "+path_start
+        mypath=path_start
+        print "Prefix: mypath: "+mypath
+        if path_start=='projects':
+          print "Prefix: in project mypath: "+mypath
+          proj_uri=environ['PATH_INFO']
+          print "Prefix: in project projiri: "+proj_uri
+          uri_parts=proj_uri.split('/')
+          mypath=uri_parts[2]
+        app = self.get_application(mypath)
+        if app is None:
+            app = self.default_app
+        return app(environ, start_response)
+
 
 from main.sqla.app import create_app, get_user_for_prefix
 #from main.web_apps_examples import *
@@ -22,6 +59,8 @@ def make_app(prefix):
     if user is None:
        return NotFound()
     return create_app(user)
+
+application = PathDispatcher(NotFound(), make_app)
 
 ## from app.sqla import *
 #from main.sqla.app import app as application
--- main/sqla/app.py
+++ main/sqla/app.py
@@ -1,61 +1,66 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# my files
-from methods import *
 
 
-
-# region Create application
-app = Flask(__name__)
-
-# region GENERAL SETTINGS
-
-# Create dummy secrey key so we can use sessions
-app.config['SECRET_KEY'] = '123456790'
-
-# Create in-memory database
-# app.config['DATABASE_FILE'] = 'sample_db.sqlite'
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
-app.config['SQLALCHEMY_DATABASE_URI'] ='{}://{}:{}@{}/{}'\
-    .format(dbconfig.db_engine,dbconfig.db_user,dbconfig.db_password, dbconfig.db_hostname,dbconfig.db_name)
-
-
-
-
-app.config.update(
-    DEBUG=dbconfig.debug
-)
-# endregion
-
-# region EMAIL SETTINGS
-
-app.secret_key = dbconfig.mail_secret_key
-app.config["MAIL_SERVER"] = dbconfig.mail_server
-app.config["MAIL_PORT"] = dbconfig.mail_port
-app.config["MAIL_USE_SSL"] = dbconfig.mail_use_ssl
-app.config["MAIL_USERNAME"] = dbconfig.mail_username
-if not dbconfig.is_server_version:  # personal machine
-    app.config["AAAS_MAIL_SENDER"] = dbconfig.mail_username
-    app.config["MAIL_PASSWORD"] = dbconfig.mail_password
-else:  # server
-    app.config["AAAS_MAIL_SENDER"] = dbconfig.mail_sender
-    app.config["MAIL_DEFAULT_SENDER"] = dbconfig.mail_sender
-
-# endregion
-
-db = SQLAlchemy(app)
-
-# endregion
-
-
-DBAS = DBAS(app,db)
-
+## region Create application
+#app = Flask(__name__)
+#
+## region GENERAL SETTINGS
+#
+## Create dummy secrey key so we can use sessions
+#app.config['SECRET_KEY'] = '123456790'
+#
+## Create in-memory database
+## app.config['DATABASE_FILE'] = 'sample_db.sqlite'
+#
+## app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
+#app.config['SQLALCHEMY_DATABASE_URI'] ='{}://{}:{}@{}/{}'\
+#    .format(dbconfig.db_engine,dbconfig.db_user,dbconfig.db_password, dbconfig.db_hostname,dbconfig.db_name)
+#
+#
+#
+#
+#app.config.update(
+#    DEBUG=dbconfig.debug
+#)
+## endregion
+#
+## region EMAIL SETTINGS
+#
+#app.secret_key = dbconfig.mail_secret_key
+#app.config["MAIL_SERVER"] = dbconfig.mail_server
+#app.config["MAIL_PORT"] = dbconfig.mail_port
+#app.config["MAIL_USE_SSL"] = dbconfig.mail_use_ssl
+#app.config["MAIL_USERNAME"] = dbconfig.mail_username
+#if not dbconfig.is_server_version:  # personal machine
+#    app.config["AAAS_MAIL_SENDER"] = dbconfig.mail_username
+#    app.config["MAIL_PASSWORD"] = dbconfig.mail_password
+#else:  # server
+#    app.config["AAAS_MAIL_SENDER"] = dbconfig.mail_sender
+#    app.config["MAIL_DEFAULT_SENDER"] = dbconfig.mail_sender
+#
+## endregion
+#
+#db = SQLAlchemy(app)
+#
+## endregion
+#
+#
+#DBAS = DBAS(app,db)
+#
 def start_app():
     pass
 
+def get_user_for_prefix(prefix):
+    print "Prefix: "+prefix
+    return prefix 
+
 def create_app(config_filename):
+    # my files
+    from methods import *
+    print "Prefix: creating app: "+config_filename
+
     app = Flask(__name__)
 #    app.config.from_pyfile(config_filename)
 
@@ -99,7 +104,7 @@ def create_app(config_filename):
     else:  # server
         app.config["AAAS_MAIL_SENDER"] = dbconfig.mail_sender
         app.config["MAIL_DEFAULT_SENDER"] = dbconfig.mail_sender
-    
+       
     # endregion
     
     db = SQLAlchemy(app)

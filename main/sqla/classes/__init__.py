2a9466a774380d01ce6ee7ac5cdd91cb65f84194
cenv0594 <gwyneth.bradbury@ouce.ox.ac.uk>
2017-09-14 11:02:46 +0100

    tables used to connect to DB now and added try/except statements to catch a lot of errors with authentication which should be kept in check but useful for updating the database

--- main/sqla/classes/__init__.py
+++ main/sqla/classes/__init__.py
@@ -4,21 +4,29 @@ import importlib
 
 
 
-def initialise(db, db_list=['map', 'it_lending_log', 'online_learning','iaas']):
+def initialise(db, db_list=['map'], dbstring_list=["mysql+pymysql://root:***@localhost/map"]):
     classesdict = {}
     my_db = {}
-    for db_item in db_list:
-        my_db[db_item] = importlib.import_module('.classes_master', __name__)
-        CL = my_db[db_item].init(db,db_item)
-        # my_db[db_item].init(db,db_item)
-        # this doesn't work as we need to map to the class, not merely the name of the class!
-        for i in CL:#my_db[db_item].class_list:
-            classes_arr = i.split('_')
-            # cn = ""
-            # for cl in classes_arr:
-            #     cn = cn + cl.capitalize()
-            cn = "cls_"+db_item+"_"+str(i)
-            classesdict["cls_" + db_item + "_" + i] = getattr(my_db[db_item], cn)
+    for i in range(len(db_list)):
+        try:
+            db_item = db_list[i]
+            dbstring=dbstring_list[i]
+            my_db[db_item] = importlib.import_module('.classes_master', __name__)
+            CL = my_db[db_item].init(db,db_item,dbstring)
+            # my_db[db_item].init(db,db_item)
+            # this doesn't work as we need to map to the class, not merely the name of the class!
+            for i in CL:#my_db[db_item].class_list:
+                classes_arr = i.split('_')
+                # cn = ""
+                # for cl in classes_arr:
+                #     cn = cn + cl.capitalize()
+                cn = "cls_"+db_item+"_"+str(i)
+                try:
+                    classesdict["cls_" + db_item + "_" + i] = getattr(my_db[db_item], cn)
+                except Exception as e:
+                    print(e)
+        except Exception as e:
+            print(e)
     return classesdict, my_db
 
 def initialise_single_class(databasename,classname):

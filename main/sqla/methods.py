8dc5cefd24398c7a8409976db42d3eac3465921c
cenv0594 <gwyneth.bradbury@ouce.ox.ac.uk>
2017-10-02 17:13:14 +0100

    -

--- main/sqla/methods.py
+++ main/sqla/methods.py
@@ -824,20 +824,20 @@ class DBAS():
 
     def dbas_admin_pages_setup(self):
 
-        binds = self.SQLALCHEMY_BINDS
-        for d in binds:
-            print(d, binds[d])
-
-            self.add_collection_of_views(d.__str__(), self.classesdict, self.class_db_dict,
-                                         svc_group=self.svc_groups[d])
-
-
-        # for s in self.services:
-        #     for db in self.services[s].db:
-        #         d = db.dbname
+        # binds = self.SQLALCHEMY_BINDS
+        # for d in binds:
+        #     print(d, binds[d])
         #
-        #         self.add_collection_of_views(d.__str__(), self.classesdict, self.class_db_dict,
-        #                                      svc_group=self.svc_groups[d])
+        #     self.add_collection_of_views(d.__str__(), self.classesdict, self.class_db_dict,
+        #                                  svc_group=self.svc_groups[d])
+
+
+        for s in self.services:
+            for db in self.services[s].db:
+                d = db.dbname
+
+                self.add_collection_of_views(d.__str__(), self.classesdict, self.class_db_dict,
+                                             svc_group=self.svc_groups[d])
 
 
 

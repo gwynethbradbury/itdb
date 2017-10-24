22472984a2e20839ffaf6a9c6cdf8a961ec224c5
cenv0594 <gwyneth.bradbury@ouce.ox.ac.uk>
2017-10-24 12:29:24 +0100

    stripped db setup function

--- setup_db.py
+++ setup_db.py
@@ -1,27 +1,7 @@
-# from danceapp.database.seed import run_seed
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 import main.iaas.iaas as iaas
-import dbconfig
 
 
 
 
-
-
-
-
-# if __name__ == '__main__':
 iaas.db.create_all()
 
-# SQL:
-# create database taskmanagement;
-
-# Python:
-# python setup_db.py
-
-# SQL:
-# use taskmanagement
-# insert into tag (name,color) values ('DBAS',3);
-# insert into tag (name,color) values ('Online Learning',2);
